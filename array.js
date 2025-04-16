05601fc1965ddda1a59d6ecc4747a68167a4ca55
mohit sharma <mohitsharma755k@gmail.com>
2025-04-16 16:05:08 +0530

    Another change

--- array.js
+++ array.js
@@ -22,7 +22,18 @@ fruit.forEach((element,index,fruit) => {
     console.log(element,index,fruit)
     
 });
+const myfruit = fruit.forEach((element,index,fruit) => {
+    return `${element} is my favourate fruit and its price is ${index}`
+}   
+)
+console.log(myfruit);
 // map function
+// Map method and foeach method are similar but map method return new array and forEach method does not return any new array.You can also check this exaample
 fruit.map((element,index,fruit)=>{
     console.log(`${element} This is index number ${index}`)
 })
+const Myarr = fruit.map((element,index,fruit)=>{
+  return(`${element} is my favourate fruit and its price is ${index}`)
+})
+console.log(Myarr);
+// filter method    
