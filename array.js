532568b4446012c9fd78839330ed016b5726d0b6
mohit sharma <mohitsharma755k@gmail.com>
2025-05-05 16:19:11 +0530

    another commit

--- array.js
+++ array.js
@@ -18,60 +18,68 @@ for (let index = 0; index <= fruit.length - 1; index++) {
 }
 
 // 5th method
-fruit.forEach((element,index,fruit) => {
-    console.log(element,index,fruit)
-    
+fruit.forEach((element, index, fruit) => {
+    console.log(element, index, fruit)
+
 });
-const myfruit = fruit.forEach((element,index,fruit) => {
+const myfruit = fruit.forEach((element, index, fruit) => {
     return `${element} is my favourate fruit and its price is ${index}`
-}   
+}
 )
 console.log(myfruit);
 // map function
 // Map method and foeach method are similar but map method return new array and forEach method does not return any new array.You can also check this exaample
-fruit.map((element,index,fruit)=>{
+fruit.map((element, index, fruit) => {
     console.log(`${element} This is index number ${index}`)
 })
-const Myarr = fruit.map((element,index,fruit)=>{
-  return(`${element} is my favourate fruit and its price is ${index}`)
+const Myarr = fruit.map((element, index, fruit) => {
+    return (`${element} is my favourate fruit and its price is ${index}`)
 })
 console.log(Myarr);
 // Here is the example of map method
-const numbers = [1,2,3,4,5,6,7,8,9,10];
-const square = numbers.map((element,index,numbers)=>{
-    return element*element
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const square = numbers.map((element, index, numbers) => {
+    return element * element
 })
 console.log(square);
 // Push method
 // Push method is used to add new element in array
-const animals = ['dog','cat','cow','sheep'];
+const animals = ['dog', 'cat', 'cow', 'sheep'];
 console.log(animals);
-animals.push('lion','tiger','elephant');
+animals.push('lion', 'tiger', 'elephant');
 console.log(animals);
 // Pop method
 // Pop method is used to remove last element of array
-const animal = ['dog','cat','cow','sheep'];
+const animal = ['dog', 'cat', 'cow', 'sheep'];
 console.log(animal);
 animal.pop();
 console.log(animal);
 // Shift method
 // Shift method is used to remove first element of array
-const animal1 = ['dog','cat','cow','sheep'];
+const animal1 = ['dog', 'cat', 'cow', 'sheep'];
 console.log(animal1);
-animal1.shift();    +
-console.log(animal1);
+animal1.shift(); +
+    console.log(animal1);
 // Unshift method
 // Unshift method is used to add new element in array at first position
-const animal2 = ['dog','cat','cow','sheep'];
+const animal2 = ['dog', 'cat', 'cow', 'sheep'];
 console.log(animal2);
-animal2.unshift('lion','tiger','elephant');
+animal2.unshift('lion', 'tiger', 'elephant');
 console.log(animal2);
 // filter method 
-const num = [1,2,3,4,5,6,7,8,9,10];
-const even = num.filter((element,index,num)=>{
-    return element%2==0
-})     
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const even = num.filter((element, index, num) => {
+    return element % 2 == 0
+})
 console.log(even);
+// Splice method
+apple = ["yellow","green","red","blue","olive"];
+console.log(apple)
+apple = apple.splice(1);
+console.log(apple)
+apple = apple.splice(1,0,"Orange","apple");
+console.log(apple)
+console.log(apple)
 // reduce method
 // const num1 = [1,2,3,4,5,6,7,8,9,10];
 // const sum = num1.reduce((accumulator,currentvalue,index,num1)=>{
